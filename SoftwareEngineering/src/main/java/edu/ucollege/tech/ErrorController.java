392ee2d076ba901fae90d42497b39bf63eb7f892
Joel <jgimbel@live.com>
2013-09-25 18:42:38 -0500

    getting error controller set up correctly

--- SoftwareEngineering/src/main/java/edu/ucollege/tech/ErrorController.java
+++ SoftwareEngineering/src/main/java/edu/ucollege/tech/ErrorController.java
@@ -1,13 +1,19 @@
 package edu.ucollege.tech;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 public class ErrorController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+	
 	@RequestMapping(value = "/error", method = RequestMethod.GET)
-	public String Error(){
-		
-	return "error";
+	public String error(Model model){	
+		logger.info("404 error found");
+		return "error";
 	}
 }
