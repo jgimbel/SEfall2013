da59c64e20bb57ee8d1ef7c2bb152df1ee608102
Joel <jgimbel@live.com>
2013-11-13 20:38:46 -0600

    mysql commit

--- SoftwareEngineering/src/main/java/edu/ucollege/tech/Connections/MySQL.java
+++ SoftwareEngineering/src/main/java/edu/ucollege/tech/Connections/MySQL.java
@@ -1,6 +1,50 @@
 package edu.ucollege.tech.Connections;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.mysql.jdbc.Driver;
 
 public class MySQL {
-	private String connection = "";
+	Connection con = null;
+    Statement st = null;
+    ResultSet rs = null;
+
+    String url = "jdbc:mysql://localhost:3306/test";
+    String user = "root";
+    String password = "loot";
+	String driver = "com.mysql.jbcd.Driver";
+	public MySQL(){
+		
+		try {
+			Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+
+        } catch (Exception ex) {
+           ex.printStackTrace();
+        } 
+	}
 	
+	public String getArticles(){
+		String toReturn = "";
+		try {
+			//Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+			Statement sts = con.createStatement();
+			sts.execute("SELECT * FROM test.article");
+	            while (rs.next()) {
+	                toReturn += rs.getInt(1);
+	                toReturn += ": ";
+	                toReturn += rs.getString(2) + "\n";
+	            }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return toReturn;
+	}
 }
